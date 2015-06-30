af31478a2c96dac5af100be328b138c3ff142861
Arun Durvasula <ohblahitsme@yahoo.com>
2015-06-30 12:41:13 +0200

    testing out model

--- scripts/demographic_models.py
+++ scripts/demographic_models.py
@@ -1,6 +1,6 @@
 from dadi import Numerics, PhiManip, Integration, Spectrum
 
-def simple_canary((nuIb0, nuIb, nuMo0, nuMo, nuCa0, TIbMo, TMoCa),
+def simple_canary((nuIb0, nuIb, nuMo0, nuMo, nuCa0, TIbMo, TMoCa, TF),
     ns, pts):
     """
     nuIb0: starting Iberian population size
@@ -11,6 +11,7 @@ def simple_canary((nuIb0, nuIb, nuMo0, nuMo, nuCa0, TIbMo, TMoCa),
     nuCa: ending Canary island population size
     TIbMo: split time for Iberian and Moroccan populations
     TMoCa: split time for Moroccan and Canary island populations
+    TF: simulation ending time
     """
     xx = Numerics.default_grid(pts)
 
@@ -20,9 +21,9 @@ def simple_canary((nuIb0, nuIb, nuMo0, nuMo, nuCa0, TIbMo, TMoCa),
     phi = PhiManip.phi_1D_to_2D(xx, phi)
     phi = Integration.two_pops(phi, xx, TMoCa, nu1=nuIb, nu2=nuMo0)
 
-    phi = PhiManip.phi_2D_to_3D_split2(xx, phi)
+    phi = PhiManip.phi_2D_to_3D_split_2(xx, phi)
     phi = Integration.three_pops(phi, xx, TF, nu1=nuIb, nu2=nuMo, nu3=nuCa0)
 
-    fs = Spectrum.from_phi(phi, ns, (xx,))
+    fs = Spectrum.from_phi(phi, ns, (xx,xx,xx))
 
     return fs
