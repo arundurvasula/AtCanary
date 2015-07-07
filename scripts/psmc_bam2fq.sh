6bc1b2d8d291f8aa8c6614df6e101ca693a545b8
Arun Durvasula <ohblahitsme@yahoo.com>
2015-07-07 17:40:25 +0200

    psmc fixes

--- scripts/psmc_bam2fq.sh
+++ scripts/psmc_bam2fq.sh
@@ -19,9 +19,9 @@ samtools mpileup -C50 -uf ${ref} ${sample1} | bcftools view -c - \
 samtools mpileup -C50 -uf ${ref} ${sample2} | bcftools view -c - \
       | vcfutils.pl vcf2fq -d 10 -D 100 | gzip > data/${sample2}.fq.gz
 
-seqtk mergefa -hq20 ${sample1}.fq.gz ${sample2}.fq.gz \
+${SW}/seqtk/seqtk mergefa -hq20 ${sample1}.fq.gz ${sample2}.fq.gz \
       | ${SW}/psmc/utils/fq2psmcfa -q30 - > data/${out}.psmcfa
 
-${SW}/psmc/psmc -N25 -t15 -r5 -p "4+25*2+4+6" -o data/${out}.psmc data/${out}.psmcfa
-${SW}/psmc/utils/psmc2history.pl data/${out}.psmc | ${SW}/psmc/utils/history2ms.pl > ms-cmd.sh
-${SW}/psmc/utils/psmc_plot.pl ../results/${out} data/${out}.psmc
+#${SW}/psmc/psmc -N25 -t15 -r5 -p "4+25*2+4+6" -o data/${out}.psmc data/${out}.psmcfa
+#${SW}/psmc/utils/psmc2history.pl data/${out}.psmc | ${SW}/psmc/utils/history2ms.pl > ms-cmd.sh
+#${SW}/psmc/utils/psmc_plot.pl ../results/${out} data/${out}.psmc
